71a52291f016071f3912666dc7c0f1a8fff459e4
Julien Palard <julien@palard.fr>
2021-10-16 21:49:45 +0200

    Add a sorting script.

---
+++ scripts/sort.py
@@ -0,0 +1,27 @@
+"""Just sort the given files.
+
+And remove blank lines.
+"""
+
+import argparse
+from pathlib import Path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("file", nargs="*")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    for file in args.file:
+        path = Path(file)
+        lines = path.read_text(encoding="UTF-8").splitlines()
+        lines = [line for line in lines if line]
+        lines.sort()
+        path.write_text("\n".join(lines) + "\n", encoding="UTF-8")
+
+
+if __name__ == '__main__':
+    main()
