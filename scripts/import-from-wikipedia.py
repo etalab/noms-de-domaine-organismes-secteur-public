5410026d7ea7d37aed7e2871e2a6e450a2df40e3
Julien Palard <julien@palard.fr>
2022-03-30 15:33:11 +0200

    import-from-wikipedia: Use the bz2 directly.

--- scripts/import-from-wikipedia.py
+++ scripts/import-from-wikipedia.py
@@ -1,9 +1,19 @@
 """Script to import .gouv.fr. domains from an XML Wikipedia dump.
+
+Uses .xml.bz2 dumps from:
+
+=> https://meta.wikimedia.org/wiki/Data_dump_torrents#French_Wikipedia
+
+Run directly on the bz2 file like:
+
+scripts/import-from-wikipedia.py frwiki-20220101-pages-articles-multistream.xml.bz2
 """
 
 import argparse
 from pathlib import Path
+import bz2
 import re
+from tqdm import tqdm
 from sort import sort_files
 
 ROOT = Path(__file__).resolve().parent.parent
@@ -16,33 +26,38 @@ def parse_args():
     return parser.parse_args()
 
 
+URL = re.compile(  # Restricted URL regex inspired from validators.
+    r"^"
+    # protocol identifier
+    r"https?://"
+    # host name
+    r"(?:(?:(?:xn--)|[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]-?)*"
+    r"[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]+)"
+    # domain name
+    r"(?:\.(?:(?:xn--)|[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]-?)*"
+    r"[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]+)*"
+    # TLD identifier
+    r"\.gouv\.fr"
+    # port number
+    r"(?::\d{2,5})?",
+    re.UNICODE | re.IGNORECASE,
+)
+
+
 def main():
     args = parse_args()
-
-    url = re.compile(  # Restricted URL regex inspired from validators.
-        r"^"
-        # protocol identifier
-        r"https?://"
-        # host name
-        r"(?:(?:(?:xn--)|[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]-?)*"
-        r"[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]+)"
-        # domain name
-        r"(?:\.(?:(?:xn--)|[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]-?)*"
-        r"[a-z\u00a1-\uffff\U00010000-\U0010ffff0-9]+)*"
-        # TLD identifier
-        r"\.gouv\.fr"
-        # port number
-        r"(?::\d{2,5})?",
-        re.UNICODE | re.IGNORECASE,
-    )
     found = set()
-    with open(args.wikipedia_dump, encoding="UTF-8") as wikipedia_dump:
-        for line in wikipedia_dump:
-            for match in url.findall(line):
-                found.add(match.split("/")[2].split(":", maxsplit=1)[0])
+    with tqdm(
+        desc="Domains found", unit="domain", position=1, total=float("inf")
+    ) as found_progress:
+        with bz2.open(args.wikipedia_dump, mode="rt", encoding="UTF-8") as dump:
+            for line in tqdm(dump, "Lines scanned", unit="line"):
+                for match in URL.findall(line):
+                    found_progress.update()
+                    found.add(match.split("/")[2].split(":", maxsplit=1)[0])
     with open(SOURCES / "gouvfr-divers.txt", "a", encoding="UTF-8") as gouvfr_divers:
         for domain in found:
-            gouvfr_divers.write(domain + "\n")
+            gouvfr_divers.write(domain + "  # (from Wikipedia dump)\n")
     sort_files([SOURCES / "gouvfr-divers.txt", SOURCES / "nongouvfr-divers.txt"])
 
 
