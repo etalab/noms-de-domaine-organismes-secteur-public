a497bae4e54b316b96b66a6910a3a6fc4f921d85
Julien Palard <julien@palard.fr>
2022-04-04 23:25:30 +0200

    Use an intermediate file as an HTTP response cache.

--- README.md
+++ README.md
@@ -16,7 +16,9 @@ scripts de collecte.
 ## La liste des URLs
 
 Le fichier `urls.txt` est une liste d’URLs basée sur les domaines du
-dossier `sources/` et répondant `200 OK` en HTTP ou en HTTPS.
+dossier `sources/` et répondant `200 OK` en HTTP ou en HTTPS
+éventuellement après une redirection sur le **même** domaine
+(typiquement l’ajout d’un `/fr/`).
 
 Les ajouts et suppresion s’y font automatiquement, il n’est pas
 nécessaire de modifier ce fichier manuellement.
@@ -38,6 +40,16 @@ Pour obtenir cette liste vous pouvez utiliser :
     export LC_COLLATE=C
     comm -13 <(cut -d/ -f3 urls.txt | sort) <(sort sources/*.txt)
 
+Il est possible de savoir ce qui cause l’innaccessibilité en regardant
+dans `domains.csv` :
+
+    $ head -n 1 domains.csv; grep mairie-valognes.fr domains.csv
+    name,http_last_check,http_status,https_last_check,https_status
+    mairie-valognes.fr,2022-04-04,301 Moved Permanently https://www.valognes.fr/,2022-04-04,[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: certificate has expired (_ssl.c:997)
+
+Ici on apprend qu’en HTTP le domaine redirige en HTTPS, et qu’en HTTPS
+le certificat est expiré.
+
 
 # Contribution
 
@@ -48,35 +60,41 @@ Pour trier le fichier que vous venez de modifier, vous pouvez utiliser :
 
     python scripts/sort.py sources/*.txt
 
-Pour vérifier que tout va bien :
+Pour vérifier la cohérence des fichiers :
 
     python scripts/check.py
 
-Et éventuellement pour consolider dans `urls.txt` (mais c’est long) :
+Et éventuellement pour consolider dans `urls.txt` :
 
-    python scripts/consolidate.py sources/*.txt
+    python scripts/http_checker.py
+    python scripts/domains_csv_to_urls_txt.py
 
-pour consolider les sources dans `urls.txt`,
-dans lequels seuls les domaines répondant en HTTP par une 200 sont
-acceptés.
+Le premier met à jour le fichier `domains.csv`, et le second crée
+`urls.txt` à partir de `domains.csv`.
 
 
 ## Scripts de collecte
 
 Le dossier `scripts/` contient plusieurs scripts de collecte :
 
-- import-base-nationale-sur-les-intercommunalites.py
-- import-from-ct-logs.py
+- `import-base-nationale-sur-les-intercommunalites.py`
+- `import-from-ct-logs.py`
+
 
 ## Exemples de réutilisations
+
 ### [sources-de-confiance.fr](https://sources-de-confiance.fr)
+
 Sources de confiance est une extension de navigateur qui permet d'identifier instantanément les résultats issus du secteur public dans son moteur de recherche habituel. Une initiative de l'association [Villes Internet](https://villes-internet.net).
 
 ### Audit d'accessibilité avec [Asqatasun](https://adullact.org/service-en-ligne-asqatasun)
+
 L'association [ADULLACT](https://adullact.org/) souhaite établir des statistiques concernant le respect du RGAA par les sites des organismes publics.
 
 ### Audits techniques variés avec [DashLord](https://dashlord.incubateur.net/intro/)
+
 DashLord est né à la [Fabrique des ministères sociaux](https://fabrique.social.gouv.fr/) pour répondre aux besoins d'évaluation et de mise en oeuvre des bonnes pratiques de développement web.
+
 
 # Licence
 
---
+++ scripts/http_checker.py
@@ -0,0 +1,182 @@
+"""Update domains.csv from sources/*.txt."""
+
+import argparse
+import asyncio
+from pathlib import Path
+import logging
+import random
+from urllib.parse import urlparse
+
+import aiohttp
+from tqdm.asyncio import tqdm
+
+from public_domain import Domain, parse_files, parse_csv_file, write_csv_file
+
+USER_AGENT = (
+    "http_checker.py (https://github.com/etalab/noms-de-domaine-organismes-publics)"
+)
+HEADERS = {"User-Agent": USER_AGENT}
+
+logger = logging.getLogger("consolidate")
+
+
+def avoid_surrogates(s):
+    return s.encode("utf-8", "surrogateescape").decode("utf-8", "backslashreplace")
+
+
+def to_message(err):
+    """Try to producuce a clean and readable message from the given HTTP
+    response or exception caused by an HTTP query."""
+    match err:
+        case aiohttp.ClientResponse(status=200):
+            return f"200 {avoid_surrogates(err.reason)}"
+        case aiohttp.ClientResponse(status=301 | 302 | 303 | 307 | 308):
+            dest = err.headers.get("Location", "(but no Location in headers)")
+            return f"{err.status} {avoid_surrogates(err.reason)} {dest}"
+        case aiohttp.client_exceptions.ClientConnectorError() if err.strerror:
+            return err.strerror
+        case aiohttp.client_exceptions.ClientConnectorCertificateError():
+            return f"{err.certificate_error!s}"
+        case asyncio.TimeoutError():
+            return "Timeout"
+        case aiohttp.ClientResponse():
+            return f"{err.status} {avoid_surrogates(err.reason)}"
+        case _:
+            return type(err).__name__ + ": " + str(err)
+
+
+def share_same_domain(url1: str, url2: str):
+    return urlparse(url1).netloc == urlparse(url2).netloc
+
+
+async def http_head(
+    url: str, client: aiohttp.ClientSession, max_redirects=10
+) -> aiohttp.ClientResponse:
+    """Performs an HTTP GET on the given URL.
+
+    Only follow redirections on the **same** domain, so:
+
+    - if http://munster.alsace redirects to http://www.munster.alsace it's a redirection.
+    - if http://munster.alsace redirects to http://muster.alsace/fr replying OK it's OK.
+    """
+    async with client.head(url, headers=HEADERS, allow_redirects=False) as response:
+        logger.info("%s: %s %s", url, response.status, response.reason)
+    if (
+        300 < response.status < 400
+        and "Location" in response.headers
+        and share_same_domain(url, response.headers["Location"])
+        and max_redirects > 0
+    ):
+        return await http_head(response.headers["Location"], client, max_redirects - 1)
+    return response
+
+
+async def check_domain(
+    domain: Domain,
+    client: aiohttp.ClientSession,
+) -> None:
+    """Check if the given domain replies an ok-ish response over HTTP or HTTPS."""
+    for protocol in "https", "http":
+        try:
+            url = f"{protocol}://{domain.name}"
+            logger.debug("%s: Querying...", url)
+            response = await http_head(url, client=client)
+            domain.set_status(protocol, to_message(response))
+        except (aiohttp.ClientError, asyncio.TimeoutError, ValueError) as err:
+            message = to_message(err)
+            domain.set_status(protocol, message)
+            logger.info("%s: KO: %s", url, message)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    here = Path(__file__).parent
+    default_files = list((here / "sources").glob("*.txt"))
+    parser.add_argument("files", type=Path, nargs="*", default=default_files)
+    parser.add_argument(
+        "--output",
+        type=Path,
+        help="File to write domains with OK HTTP responses.",
+        default=here.parent / "domains.csv",
+    )
+    parser.add_argument(
+        "--slow",
+        help="Run slower, in case we're rate limited.",
+        action="store_const",
+        const=1,
+        default=0,
+        dest="kindness",
+    )
+    parser.add_argument(
+        "--slower",
+        help="Run even slower, in case we're rate limited.",
+        action="store_const",
+        const=2,
+        default=0,
+        dest="kindness",
+    )
+    parser.add_argument(
+        "--slowest",
+        help="Run extremly slowly, in case we're rate limited.",
+        action="store_const",
+        const=3,
+        default=0,
+        dest="kindness",
+    )
+    parser.add_argument("--limit", help="Query at most n domains.", type=int)
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="Verbosity: use -v or -vv.",
+    )
+    parser.add_argument(
+        "-s", "--silent", action="store_true", help="Disable progress bar"
+    )
+    args = parser.parse_args()
+    args.verbose = min(args.verbose, 2)
+    if args.limit is None:
+        args.limit = 2**32
+    return args
+
+
+async def rescan_domains(
+    to_check: list[Domain], kindness: int = 0, verbose: int = 0, silent: bool = False
+) -> None:
+    sem = asyncio.Semaphore([20, 10, 5, 2][kindness])
+
+    async def with_sem(coroutine):
+        async with sem:
+            return await coroutine
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=20)) as client:
+        gather = asyncio.gather if (verbose or silent) else tqdm.gather
+        await gather(*[with_sem(check_domain(domain, client)) for domain in to_check])
+
+    await asyncio.sleep(
+        0.5  # See https://docs.aiohttp.org/en/stable/client_advanced.html#graceful-shutdown
+    )  # Which is fixed in aiohttp 4.
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(
+        level=[logging.WARNING, logging.INFO, logging.DEBUG][args.verbose]
+    )
+    domains = parse_csv_file(args.output) | parse_files(*args.files)
+
+    to_check = sorted(
+        domains, key=lambda domain: min(domain.http_last_check, domain.https_last_check)
+    )[: args.limit]
+
+    try:
+        asyncio.run(rescan_domains(to_check, args.kindness, args.verbose, args.silent))
+    except KeyboardInterrupt:
+        logging.info("Interrupted by keyboard, saving before exiting…")
+
+    write_csv_file(args.output, domains)
+
+
+if __name__ == "__main__":
+    main()
